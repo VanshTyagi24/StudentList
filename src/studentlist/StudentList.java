59cd3527457f3ec248ad1e66fabc5be9924b73dd
hp <hp@192.168.0.20>
2021-01-28 11:41:05 -0500

    First Commit

---
+++ src/studentlist/StudentList.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package studentlist;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Vansh
+ */
+public class StudentList {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Student[] students = new Student[2];
+        
+        Scanner input = new Scanner(System.in);
+        
+        for( int i = 0; i<students.length; i++){
+            System.out.println("Enter the students name");
+            String name = input.nextLine();
+            Student student = new Student(name);
+           
+            students[i] = student;
+            
+        }
+        System.out.println("Printing the students:");
+        String format = "The students name is %s\n";
+        
+        for(Student student: students){
+            System.out.printf(format, student.getName());
+        }
+    }
+    
+}
